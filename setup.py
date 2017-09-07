b4cd872e914a70b5282fcc1e51ce0c0072bb13f7
erickhein <erickhv@gmail.com>
2017-09-07 15:53:57 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from setuptools import setup
+
+setup(name='emblue',
+      version='0.1',
+      description='Utilizar el api de emblue',
+      url='http://github.com/storborg/funniest',
+      author='Erick Hein',
+      author_email='erickhv@gmail.com',
+      license='MIT',
+      packages=['emblue'],
+      zip_safe=False)
